302add3f7ed7114faaa8c60b156c06b39f5686bd
MINT <mint@inslens.com>
2019-02-19 11:46:35 +0800

    plugin

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,26 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import "../assets/style.css";
+import colors from "vuetify/es5/util/colors";
+import "vuetify/src/stylus/app.styl";
+import zhHans from "vuetify/es5/locale/zh-Hans";
+import "./confirm-dialog/index";
+import "./snackbar/index";
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
-})
+  theme: {
+    primary: colors.indigo.lighten2,
+    secondary: "#424242",
+    accent: colors.indigo.lighten2,
+    error: "#FF5252",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FFC107"
+  },
+  customProperties: true,
+  iconfont: "md",
+  lang: {
+    locales: { zhHans },
+    current: "zh-Hans"
+  }
+});
