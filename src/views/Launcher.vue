9c58263837043f2b8c892ca8eb755316864dd87f
MINT <mint@inslens.com>
2019-03-03 17:51:54 +0800

    inslens->insdim

--- src/views/Launcher.vue
+++ src/views/Launcher.vue
@@ -3,7 +3,7 @@
     <v-layout justify-center align-center fill-height>
       <v-flex xs3>
         <v-card dark flat tile ripple :aspect-ratio="1/1" to="/inslens">
-          <v-card-title>INSLENS</v-card-title>
+          <v-card-title>INSDIM</v-card-title>
         </v-card>
       </v-flex>
       <v-flex xs3>
